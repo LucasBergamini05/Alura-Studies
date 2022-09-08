d361b95a8751253f9703e0fd64e53cbe58a36f60
Lucas <lucas.bergamini05@outlook.com>
2022-09-08 15:19:13 -0300

    Implementando State no Formulario

--- src/components/Formulario/index.tsx
+++ src/components/Formulario/index.tsx
@@ -3,9 +3,19 @@ import Botao from "../Botao";
 import style from './Formulario.module.scss';
 
 export default class Formulario extends Component{
+  state = {
+    tarefa: "",
+    tempo: "00:00:00"
+  }
+
+  private adicionaTarefa(event: React.FormEvent<HTMLFormElement>){
+    event.preventDefault();
+    console.log(this.state);
+  }
+
   render(): ReactNode {
     return (
-      <form className={style.novaTarefa}>
+      <form className={style.novaTarefa} onSubmit={this.adicionaTarefa.bind(this)}>
         <div className={style.inputContainer}>
           <label 
             htmlFor="tarefa">
@@ -15,6 +25,8 @@ export default class Formulario extends Component{
             type="text"
             id="tarefa"
             name="tarefa"
+            value={this.state.tarefa}
+            onChange={event => this.setState({...this.state, tarefa: event.target.value})}
             placeholder="O que você quer estudar?"
             required
           />
@@ -28,6 +40,8 @@ export default class Formulario extends Component{
             type="time"
             id="tempo"
             name="tempo"
+            value={this.state.tempo}
+            onChange={event => this.setState({...this.state, tempo: event.target.value})}
             step="1"
             min="00:00:00"
             max="02:00:00"
