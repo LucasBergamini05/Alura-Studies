d3a8db901e6a4cbc68af6def8078188624d466ef
Lucas <lucas.bergamini05@outlook.com>
2022-09-16 17:55:18 -0300

    Implementando interrupção de contagem

--- src/components/Cronometro/index.tsx
+++ src/components/Cronometro/index.tsx
@@ -4,6 +4,7 @@ import style from './Cronometro.module.scss'
 import Itarefa from '../../types/tarefa';
 import { useEffect, useState } from 'react';
 import { TimeToSeconds } from '../../common/util/data';
+import useInterval from '../../common/util/useInterval';
 
 interface Props {
   selecionado: Itarefa | undefined
@@ -11,32 +12,50 @@ interface Props {
 }
 
 export default function Cronometro({selecionado, finalizaTarefa}: Props) {
-  const [tempo, setTempo] = useState<number>();
-
+  const [tempo, setTempo] = useState(0);
+  const [contando, setContando] = useState(false);
+  
   useEffect(()=>{
     if(selecionado?.tempo) 
       return setTempo(TimeToSeconds(selecionado?.tempo));
     setTempo(0);
   }, [selecionado]);
 
-  function regressiva(tempo: number = 0){
-    setTimeout(() => {
-      if(tempo > 0) {
-        setTempo(--tempo);
-        return regressiva(tempo)
-      }
-      finalizaTarefa()
-    }, 1000);
+  useInterval({callback: regressiva, delay: contando? 1000 : null});
+  
+  function controlaRegressiva(controle: boolean){
+    if(selecionado){
+      setContando(controle);
+      if(!controle)
+        setTempo(TimeToSeconds(selecionado?.tempo));
+    }
   }
 
+  function regressiva(){
+    console.log(tempo)
+    if(tempo > 0)
+      return setTempo(tempo-1); 
+
+    setContando(false);
+    finalizaTarefa();
+  }
+  
   return (
     <div className={style.cronometro}>
       <p className={style.titulo}>Escolha um card e inicie o cronômetro</p>
       <div className={style.relogioWrapper}>
         <Relogio tempo={tempo}/>
       </div>
-      <Botao onClick={()=>{regressiva(tempo)}}>
-        Começar
+      <Botao onClick= 
+      {
+        contando ? 
+        ()=> {
+          controlaRegressiva(false);
+        } : ()=> {
+          controlaRegressiva(true);
+        }
+      }>
+        {contando ? 'Parar' : 'Começar'}
       </Botao>
     </div>
   )
